a1f6d37ecd32008d53de836d487dd744d9ca429e
xshigeox <59002383+xshigeox@users.noreply.github.com>
2020-03-02 13:43:37 -0500

    unused switch import removed from app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Switch, BrowserRouter } from "react-router-dom"
+import { Route, BrowserRouter } from "react-router-dom"
 import NavBar from "./NavBar"
 
 const App = props => {
