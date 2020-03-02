9015756704553921a9807c6e8cd219fe1a969e28
xshigeox <59002383+xshigeox@users.noreply.github.com>
2020-03-02 16:09:44 -0500

    create adopt me form.  validation for inputs. import lodash

---
+++ src/components/AdoptMeForm.js
@@ -0,0 +1,124 @@
+import React, { useState } from "react"
+import _ from "lodash"
+import ErrorList from "./ErrorList"
+
+const AdoptMeForm = props => {
+  const defaultFormValue = {
+    name: "",
+    phoneNumber: "",
+    email: "",
+    homeStatus: ""
+  }
+
+  const [newApplication, setNewApplication] = useState(defaultFormValue)
+  const [errors, setErrors] = useState({})
+
+  const validForSubmission = () => {
+    let submitErrors = {}
+    const requiredFields = ["name", "phoneNumber", "email", "homeStatus"]
+
+    requiredFields.forEach(field => {
+      if (newApplication[field].trim() === "") {
+        submitErrors = {
+          ...submitErrors,
+          [field]: "is blank"
+        }
+      }
+    })
+
+    setErrors(submitErrors)
+    return _.isEmpty(submitErrors)
+  }
+
+  const handleInputChange = event => {
+    setNewApplication({
+      ...newApplication,
+      [event.currentTarget.id]: event.currentTarget.value
+    })
+  }
+
+  const onSubmitHandler = event => {
+    event.preventDefault()
+    if (validForSubmission()) {
+      props.handleSubmit(newApplication)
+      setNewApplication(defaultFormValue)
+    }
+  }
+
+  const homeList = ["Own", "Rent"]
+  const homeStatus = [""].concat(homeList).map(status => {
+    return (
+      <option key={status} value={status}>
+        {status}
+      </option>
+    )
+  })
+
+  return (
+    <form
+      autoComplete="off"
+      it="adoptMeForm"
+      className="callout"
+      onSubmit={onSubmitHandler}
+    >
+      <h1>Adopt Me!</h1>
+      <ErrorList errors={errors} />
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={newApplication.name}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="phoneNumber">Phone Number:</label>
+        <input
+          type="text"
+          id="phoneNumber"
+          name="phoneNumber"
+          value={newApplication.phoneNumber}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          value={newApplication.email}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="homeStatus">Home Status:</label>
+        <select
+          id="homeStatus"
+          onChange={handleInputChange}
+          value={newApplication.homeStatus}
+        >
+          {homeStatus}
+        </select>
+      </div>
+
+      <input
+        type="hidden"
+        name="applicationStatus"
+        id="applicationStatus"
+        value="pending"
+      />
+
+      <input type="hidden" name="petId" id="petId" value={props.petId} />
+
+      <input type="submit" className="button" value="submit" />
+    </form>
+  )
+}
+
+export default AdoptMeForm
