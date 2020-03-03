71b14f93d42114b5f90aee554792b55b79056eaa
xshigeox <59002383+xshigeox@users.noreply.github.com>
2020-03-03 11:31:33 -0500

    removed unused usestate import

--- src/__tests__/AdoptMeForm.test.js
+++ src/__tests__/AdoptMeForm.test.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React from "react"
 import { BrowserRouter } from "react-router-dom"
 import Enzyme, { mount } from "enzyme"
 import Adapter from "enzyme-adapter-react-16"
