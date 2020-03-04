f5eefb59f048b84220951b21443609b7853d0cdf
Stephanie Garcia <stephaniegarcia@Stephanie-Garcias-MacBook-Pro.local>
2020-03-04 15:07:06 -0500

    Added error handling

--- src/components/ShowPage.js
+++ src/components/ShowPage.js
@@ -2,12 +2,21 @@ import React, { useState, useEffect} from "react"
 import AnimalInfo from "./AnimalInfo"
 
 const ShowPage = (props) =>  {
-    
+
     const [pet, setPet] = useState({})
     const petId = props.match.params.id 
 
     useEffect(()  =>  {
         fetch(`/api/v1/pets/${petId}`)
+        .then((response)  =>  {
+            if (response.ok)  {
+                return response 
+            } else  {
+                let errorMessage = `${response.status} (${response.statusText})`,
+                error = new Error(errorMessage)
+                throw (error)
+            }
+        })
         .then(response  => response.json())
         .then(fetchedPet  =>  {
             setPet(fetchedPet)
