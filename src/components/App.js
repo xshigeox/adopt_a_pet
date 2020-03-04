be841869c3c9677e100ea863b28dbaafe416763e
xshigeox <59002383+xshigeox@users.noreply.github.com>
2020-03-04 08:46:02 -0500

    Moved navbar components in to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,46 @@
 import React from "react"
-import { Route, BrowserRouter } from "react-router-dom"
-import NavBar from "./NavBar"
+import { Route, BrowserRouter, Link, Switch } from "react-router-dom"
+import SurrenderForm from "./SurrenderForm"
 
 const App = props => {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={NavBar} />
+      <div className="top-links">
+        <nav className="top-bar topbar-responsive">
+          <div className="top-bar-title">
+            <Link to="/pets" id="topbar-site-title">
+              <strong>Pet Adoption Site</strong>
+            </Link>
+          </div>
+          <div id="topbar-responsive" className="topbar-responsive-links">
+            <div className="top-bar-right">
+              <ul className="menu simple vertical medium-horizontal">
+                <li>
+                  <Link to="/pets">Home</Link>
+                </li>
+                <li>
+                  <Link to="/guineapigs">Guinea Pigs</Link>
+                </li>
+                <li>
+                  <Link to="/reptiles">Reptiles</Link>
+                </li>
+                <li>
+                  <Link to="/adoptions/new">Put up for Adoption</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      <div>
+        <Switch>
+          <Route exact path="/pets" />
+          <Route exact path="/guineapigs" />
+          <Route exact path="/reptiles" />
+          <Route exact path="/adoptions/new" component={SurrenderForm} />
+        </Switch>
+      </div>
     </BrowserRouter>
   )
 }
