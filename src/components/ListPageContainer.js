cdfb42b8f860c6b285b6597d287553f9d7fbb8f8
JSENL <jasenlambright@gmail.com>
2020-03-05 17:25:28 -0500

    updated changes

--- src/components/ListPageContainer.js
+++ src/components/ListPageContainer.js
@@ -1,51 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from "react"
-import ListPage from "./ListPage"
-
-const ListPageContainer = props => {
-  const [lists, setLists] = useState([])
-
-  const reptile = "guineapig"
-
-  useEffect(() => {
-    fetch(`/api/v1/pets/${reptile}`)
-      .then(response => {
-        if (response.ok) {
-          return response
-        } else {
-          let errorMessage = `${response.status} (${response.statusText})`,
-            error = new Error(errorMessage)
-          throw error
-        }
-      })
-      .then(result => {
-        return result.json()
-      })
-      .then(json => {
-        console.log(`RESULT JSON: ${json}`)
-        setLists(json)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }, [])
-
-  const listOfPets = lists.map(element => {
-    return <ListPage data={element} />
-  })
-
-  return (
-    <div className="pets-you-might-know">
-      <div className="add-pets-header">
-        <h6 className="header-title">Give them a Forever Home</h6>
-      </div>
-      {listOfPets}
-    </div>
-  )
-}
-
-export default ListPageContainer
-=======
 import React, { useState, useEffect } from "react"
 import ListPage from "./ListPage"
 
@@ -92,4 +44,3 @@ const ListPageContainer = props => {
 }
 
 export default ListPageContainer
->>>>>>> d6da04787cdab7758319dff9c67b6c5ab0dede5e
