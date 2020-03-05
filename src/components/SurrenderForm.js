1b4285a99fb1872eca8b22f48672e14502566139
Juvenal Miranda <juvenalmirandajr@hotmail.com>
2020-03-05 16:04:30 -0500

    added styling in headers

--- src/components/SurrenderForm.js
+++ src/components/SurrenderForm.js
@@ -98,7 +98,7 @@ const SurrenderForm = props => {
         className="callout"
         onSubmit={addNewPet}
       >
-        <h1>Surrender a Pet</h1>
+        <h1 className="header-title">Surrender a Pet</h1>
         <ErrorList errors={errors} />
         <div>
           <label htmlFor="name">Name:</label>
