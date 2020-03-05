6c9f27b7888721d6f8f78ba88b123e312ef216de
Stephanie Garcia <stephaniegarcia@Stephanie-Garcias-MacBook-Pro.local>
2020-03-05 14:14:23 -0500

    Merge branch 'master' of https://github.com/xshigeox/adopt_a_pet into pending-app-list-page

--- db/seed.sql
+++ db/seed.sql
@@ -6,7 +6,10 @@ INSERT INTO adoptable_pets (name, img_url, age, vaccination_status, adoption_sto
 ('Helena', 'https://www.reptilecentre.com/images/wmfixed/Reptile/frozen-guinea-pig-v1-495-495.jpg?v=2', 3, TRUE,'Is a sweet guineapig that loves belly rubs.', 'Pending', 1),
 ('Artemis', 'https://lafeber.com/vet/wp-content/uploads/Veiled-chameleon-by-Mrs-Logic-cropped-square.jpg', 5, FALSE,'Is a chameleon that gets excited at the sight of lettuce.', 'Pending', 2),
 ('Rocky', 'https://cdn.mos.cms.futurecdn.net/gJJFamQca86CibEeDmegk-1024-80.jpg', 7, FALSE,'Is a ornery guineapig that hates belly rubs.', 'Pending', 1),
-('Harry', 'https://img1.grunge.com/img/gallery/the-truth-about-why-there-arent-snakes-in-ireland/intro-1547479326.jpg', 2, TRUE,'Is a snake with a huge appetite for bunnies.', 'Pending', 2);
+('Harry', 'https://img1.grunge.com/img/gallery/the-truth-about-why-there-arent-snakes-in-ireland/intro-1547479326.jpg', 2, TRUE,'Is a snake with a huge appetite for bunnies.', 'Pending', 2),
+('Sticky', 'https://live.staticflickr.com/8576/15166690374_6dcf167afa_b.jpg', 1, TRUE,'This guy loves cickets', 'Pending', 2),
+('Pancake', 'https://i.pinimg.com/originals/f8/bc/80/f8bc8082646c8bc7e7a87b9d6b02543f.jpg', 4, TRUE,'This guy loves to lounge around', 'Pending', 1),
+('Dino', 'https://www.zillarules.com/-/media/images/zilla-na/us/blog-articles/myths-facts-about-bearded-dragon-care/bearded-dragon.jpg?h=350&w=350&la=en&hash=BB45C333590D6C0AF6E426FC419D6A3B224212BE', 3, TRUE,'Great lizard just looking to play.', 'Pending', 2);
 
 INSERT INTO pet_surrender_applications (name, phone_number, email, pet_name, pet_age, pet_type_id, pet_img_url, vaccination_status, application_status) VALUES
 ('John', 6178825647, 'JohnM@gmail.com', 'Bob', 2, 2, 'snakeimg.com', FALSE , 'pending'),
