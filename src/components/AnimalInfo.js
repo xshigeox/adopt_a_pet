7a51e81cfad735aa739a7db4d191f20274c07d24
JSENL <jasenlambright@gmail.com>
2020-03-06 09:27:47 -0500

    Updated indentation

--- src/components/AnimalInfo.js
+++ src/components/AnimalInfo.js
@@ -2,73 +2,73 @@ import React, { useState } from "react"
 import AdoptMeForm from "./AdoptMeForm"
 
 const AnimalInfo = props => {
-  const [formReveal, setFormReveal] = useState(false)
-  let name
-  let age
-  let img_url
-  let vaccinationStatus
-  let adoptionStory
-  let petId
-  let vStatus
+	const [formReveal, setFormReveal] = useState(false)
+	let name
+	let age
+	let img_url
+	let vaccinationStatus
+	let adoptionStory
+	let petId
+	let vStatus
 
-  if (props.pet[0]) {
-    name = props.pet[0].name
-    age = props.pet[0].age
-    img_url = props.pet[0].img_url
-    vaccinationStatus = props.pet[0].vaccination_status
-    adoptionStory = props.pet[0].adoption_story
-    petId = props.pet[0].id
-  }
+	if (props.pet[0]) {
+		name = props.pet[0].name
+		age = props.pet[0].age
+		img_url = props.pet[0].img_url
+		vaccinationStatus = props.pet[0].vaccination_status
+		adoptionStory = props.pet[0].adoption_story
+		petId = props.pet[0].id
+	}
 
-  if (vaccinationStatus === true) {
-    vStatus = "Up to Date"
-  }
+	if (vaccinationStatus === true) {
+		vStatus = "Up to Date"
+	}
 
-  const onClickHandler = event => {
-    setFormReveal(true)
-  }
+	const onClickHandler = event => {
+		setFormReveal(true)
+	}
 
-  if (props.pet[0]) {
-    return (
-      <div>
-        <div className="portfolio-resume-scrolling-container row">
-          <div
-            className="columns small-12 medium-5 portfolio-resume-overview"
-            style={{
-              backgroundSize: "cover",
-              zIndex: 0,
-              backgroundImage: `url(${img_url})`
-            }}
-          >
-            <div className="portfolio-resume-overview-content"></div>
-          </div>
-          <div className="columns small-12 medium-7 portfolio-resume-scrolling">
-            <h3>{name}</h3>
-            <h5>Age: {age}</h5>
+	if (props.pet[0]) {
+		return (
+			<div>
+				<div className="portfolio-resume-scrolling-container row">
+					<div
+						className="columns small-12 medium-5 portfolio-resume-overview"
+						style={{
+							backgroundSize: "cover",
+							zIndex: 0,
+							backgroundImage: `url(${img_url})`
+						}}
+					>
+						<div className="portfolio-resume-overview-content"></div>
+					</div>
+					<div className="columns small-12 medium-7 portfolio-resume-scrolling">
+						<h3>{name}</h3>
+						<h5>Age: {age}</h5>
 
-            <h5>Vaccination Status:</h5>
-            <p>{vStatus}</p>
+						<h5>Vaccination Status:</h5>
+						<p>{vStatus}</p>
 
-            <h5>Adoption Story:</h5>
-            <p>{adoptionStory}</p>
+						<h5>Adoption Story:</h5>
+						<p>{adoptionStory}</p>
 
-            <button
-              className="button primary expanded"
-              type="button"
-              onClick={onClickHandler}
-            >
-              Adopt Me
-            </button>
-          </div>
-        </div>
-        <div>
-          <AdoptMeForm formReveal={formReveal} petId={petId} />
-        </div>
-      </div>
-    )
-  } else {
-    return ""
-  }
+						<button
+							className="button primary expanded"
+							type="button"
+							onClick={onClickHandler}
+						>
+							Adopt Me
+						</button>
+					</div>
+				</div>
+				<div>
+					<AdoptMeForm formReveal={formReveal} petId={petId} />
+				</div>
+			</div>
+		)
+	} else {
+		return ""
+	}
 }
 
 export default AnimalInfo
